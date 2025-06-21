ffafe1deadd8f0563a12f764a810145f43d4e29a
admin <admin@Renan.local>
2025-06-21 16:27:47 -0300

    chore: initial setup 🚀

---
+++ src/hello.ts
@@ -0,0 +1,7 @@
+import { HelloController } from "./application/controllers/HelloController";
+
+const controller = new HelloController();
+
+export async function handler() {
+    return await controller.handle();
+}
